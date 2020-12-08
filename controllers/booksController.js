98fb09920d41d1201f148acbd2443cc537388557
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-07 23:07:53 -0500

    set up error codes and controllers in routes

---
+++ controllers/booksController.js
@@ -0,0 +1,29 @@
+// dev + var
+const db = require("../models")
+
+// set up controller in routes
+module.exports = {
+    findAll: (req, res) => {
+        db.Book.find(req.query).sort({ date: -1 }).then(books => {
+            res.json(books)
+        }).catch(err => {
+            res.status(404).json(err)
+        })
+    },
+    create: (req, res) => {
+        db.Book.create(req.body).then(book => {
+            res.json(book)
+        }).catch(err => {
+            res.status(404).json(err)
+        })
+    },
+    remove: (req, res) => {
+        db.Book.findById({ _id: req.params.id }).then(book => {
+            book.remove()
+        }).then(book => {
+            res.json(book)
+        }).catch(err => {
+            res.status(404).json(err)
+        })
+    }
+}
