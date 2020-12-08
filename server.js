0544601428385abe6fa94793fe874ceafee2b99b
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-07 22:39:27 -0500

    middleware install and react/api route requests

--- server.js
+++ server.js
@@ -10,3 +10,27 @@ const routes = require("./routes")
 
 const PORT = process.env.PORT || 3001;
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static Assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Connect to Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/");
+
+// Define API routes here
+app.use(routes)
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, //"./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
